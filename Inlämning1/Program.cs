6b19f509f0b3ccf59e72b1dcb800b7c7c3263b7c
osla08001 <osla08001@E-PW04SMBR>
2025-09-03 12:59:57 +0200

    Add project files.

---
+++ Inlämning1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inlämnings_uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Skriver ut text
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+
+            //Ändrar texten till svart för att inte se vart programmet är localiserat.
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
